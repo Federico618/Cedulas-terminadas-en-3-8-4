f68b6fa0347c16a7e936977ebfc4474f2174c234
Unknown <federicogallego616@gmail.com>
2018-02-19 00:13:40 -0500

    Eficiencia energetica

---
+++ 12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define kwtotal 10000
+using namespace std;
+void consumo(float cons){
+
+    cout<<  "Ingrese su consumo: ";
+    cin>> cons;
+    float a;
+    a = (cons / kwtotal) * 100;
+    if(a >= 80 && a <= 100){
+            cout<< "CONSUMO MEDIO" ;
+    }
+    else if(a > 100){
+    cout<< "ALTO CONSUMO DE ENERGIA";
+     }
+     }
+     int main(){
+         float cons;
+         consumo(cons);
+         return 0; }
